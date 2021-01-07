1095085db57050b40baf1a5e43d4d422efd69817
samsuccess <samsuccess36@gmail.com>
2021-01-07 21:00:16 +0300

    change1 homework1

--- app/src/main/java/ru/example/myapplication1/MainActivity.java
+++ app/src/main/java/ru/example/myapplication1/MainActivity.java
@@ -2,7 +2,12 @@ package ru.example.myapplication1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout linearLayout = findViewById(R.id.linerlay);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linearLayout.setBackgroundResource(R.drawable.newbackground);
+            }
+        });
     }
+
+
 }
